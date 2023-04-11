e2af5562e6a2d5758643b7388701e6b5a7269a4c
k2on <k2on@users.noreply.github.com>
2023-04-11 23:19:09 +0000

    deploy: 85cf10103c8b625815de684c5e8ebac09bafcd6b

--- nertz-5f8d8bdd898a604e.js
+++ nertz-5f8d8bdd898a604e.js
@@ -641,12 +641,12 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper293 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 115, __wbg_adapter_18);
+    imports.wbg.__wbindgen_closure_wrapper319 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 121, __wbg_adapter_18);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper638 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 238, __wbg_adapter_21);
+    imports.wbg.__wbindgen_closure_wrapper672 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 243, __wbg_adapter_21);
         return addHeapObject(ret);
     };
 
@@ -684,7 +684,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('nertz-ea62bf68149136e6_bg.wasm', import.meta.url);
+        input = new URL('nertz-5f8d8bdd898a604e_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
