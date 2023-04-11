d573c17e5a4630f7ad1eea7b0c550c7685e2837f
Max Koon <max@koon.dev>
2023-04-11 18:04:31 -0400

    fix: players are back

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,16 @@ use gloo::{
 use serde::{Deserialize, Serialize};
 use serde_json::to_string;
 use web_sys::HtmlInputElement;
-use yew::prelude::*;
+use yew::{html::Scope, prelude::*};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct State {
-    players: Vec<String>,
+    players: Vec<Player>,
     scores: Vec<Vec<Score>>,
+    is_in_progress: bool,
 }
+
+type Player = String;
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct Score {
     val: Option<i8>,
@@ -29,6 +32,7 @@ impl State {
         Self {
             players: Vec::new(),
             scores: Vec::new(),
+            is_in_progress: false,
         }
     }
 
@@ -48,6 +52,10 @@ const KEY: &str = "yew.nertzpro.self";
 
 pub enum AppMsg {
     ScoreEnter(usize, usize, i8),
+    GameNew,
+    GameStart,
+    PlayerAdd(String),
+    PlayerRemove(usize),
 }
 
 impl App {
@@ -75,6 +83,47 @@ impl App {
             }
         }
     }
+
+    fn view_input(&self, link: &Scope<Self>) -> Html {
+        let onkeypress = link.batch_callback(|e: KeyboardEvent| {
+            if e.key() == "Enter" {
+                let input: HtmlInputElement = e.target_unchecked_into();
+                let value = input.value();
+                input.set_value("");
+                Some(AppMsg::PlayerAdd(value))
+            } else {
+                None
+            }
+        });
+        html! {
+            <input
+                class="new-player"
+                placeholder="Player Name"
+                {onkeypress}
+            />
+        }
+    }
+
+    fn view_player(&self, idx: usize, player: &Player, link: &Scope<App>) -> Html {
+        let onclick = link.callback(move |_| AppMsg::PlayerRemove(idx));
+        html! {
+            <li>
+                <label>{player}</label>
+                <button {onclick}>{"x"}</button>
+            </li>
+        }
+    }
+}
+
+fn make_refs(state: &State) -> HashMap<String, NodeRef> {
+    let mut refs = HashMap::new();
+    for (round_idx, round) in state.scores.iter().enumerate() {
+        for (player_idx, _) in round.iter().enumerate() {
+            let key = format!("{}_{}", round_idx, player_idx);
+            refs.insert(key, NodeRef::default());
+        }
+    }
+    refs
 }
 
 impl Component for App {
@@ -84,34 +133,18 @@ impl Component for App {
     fn create(ctx: &Context<Self>) -> Self {
         let mut state = LocalStorage::get(KEY).unwrap_or_else(|_| State::new());
 
-        state.players.push("Max".to_owned());
-        state.players.push("Bella".to_owned());
+        let refs = make_refs(&state);
 
-        state.scores.push(vec![
-            Score {
-                val: None,
-                is_editing: true,
-            },
-            Score {
-                val: None,
-                is_editing: false,
-            },
-        ]);
-
-        Self {
-            state,
-            refs: HashMap::from([
-                (String::from("0_0"), NodeRef::default()),
-                (String::from("0_1"), NodeRef::default()),
-            ]),
-        }
+        Self { state, refs }
     }
 
     fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
-        let node_ref = self.refs.get(&self.get_focused()).unwrap();
+        if self.state.is_in_progress {
+            let node_ref = self.refs.get(&self.get_focused()).unwrap();
 
-        if let Some(input) = node_ref.cast::<HtmlInputElement>() {
-            input.focus();
+            if let Some(input) = node_ref.cast::<HtmlInputElement>() {
+                input.focus();
+            }
         }
     }
 
@@ -128,62 +161,108 @@ impl Component for App {
                     self.next_round();
                 }
             }
+            AppMsg::GameNew => {
+                let players = self.state.players.clone();
+                self.state = State::new();
+                self.state.players = players;
+            }
+            AppMsg::GameStart => {
+                self.state.is_in_progress = true;
+                self.next_round();
+            }
+            AppMsg::PlayerAdd(name) => self.state.players.push(name),
+            AppMsg::PlayerRemove(idx) => {
+                self.state.players.remove(idx);
+            }
         }
         LocalStorage::set(KEY, &self.state).expect("failed to set");
         true
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
+        console::log!(to_string(&format!("{:?}", self.state)).unwrap());
+
         html! {
-            <table>
-
-            <tr>
-                { for self.state.players.iter().map(|player| html! { <td>{player.clone().chars().nth(0).unwrap().to_uppercase()}</td> }) }
-            </tr>
-
-            { for self.state.scores.iter().enumerate().map(|(round_idx, round)| html! {
-                <tr>
-                { for round.iter().enumerate().map(|(player_idx, score)| {
-
-                    let key = format!("{}_{}", round_idx, player_idx);
-                    let node_ref = self.refs.get(&key).unwrap();
-
-                    let onkeypress = ctx.link().batch_callback(move |e: KeyboardEvent| {
-                        if e.key() == "Enter" {
-                            let input: HtmlInputElement = e.target_unchecked_into();
-                            let val = input.value().parse::<i8>().unwrap();
-                            Some(AppMsg::ScoreEnter(round_idx, player_idx, val))
-                        } else {
-                            None
-                        }
-                    });
-
+            <div>
+                <img id="logo" src="static/logo.png" alt="NERTS.PRO"/>
+                {if self.state.is_in_progress {
                     html! {
-                        <td>
-                        {if score.is_editing {
-                            html! {
-                                <input ref={node_ref} {onkeypress} value={if let Some(s) = score.val { s.to_string() } else { String::new() }} type="number"/>
-                            }
-                        } else {
-                            html! {
-                                {if let Some(s) = score.val {
-                                    html! {
-                                        {s.to_string()}
-                                    }
-                                } else {
-                                    html! {
-                                        {"--"}
-                                    }
-                                }}
-                            }
-                        }}
-                        </td>
-                    }
-                })}
-                </tr>
-            }) }
+                        <div>
+                <table class="scores">
 
-            </table>
+                <tr>
+                    { for self.state.players.iter().map(|player| html! { <td>{player.clone().chars().nth(0).unwrap().to_uppercase()}</td> }) }
+                </tr>
+
+                { for self.state.scores.iter().enumerate().map(|(round_idx, round)| html! {
+                    <tr>
+                    { for round.iter().enumerate().map(|(player_idx, score)| {
+
+                        let key = format!("{}_{}", round_idx, player_idx);
+                        let node_ref = self.refs.get(&key).unwrap();
+
+                        let onkeypress = ctx.link().batch_callback(move |e: KeyboardEvent| {
+                            if e.key() == "Enter" {
+                                let input: HtmlInputElement = e.target_unchecked_into();
+                                let val = input.value().parse::<i8>().unwrap();
+                                Some(AppMsg::ScoreEnter(round_idx, player_idx, val))
+                            } else {
+                                None
+                            }
+                        });
+
+                        html! {
+                            <td>
+                            {if score.is_editing {
+                                html! {
+                                    <input ref={node_ref} {onkeypress} value={if let Some(s) = score.val { s.to_string() } else { String::new() }} type="number"/>
+                                }
+                            } else {
+                                html! {
+                                    {if let Some(s) = score.val {
+                                        html! {
+                                            {s.to_string()}
+                                        }
+                                    } else {
+                                        html! {
+                                            {"--"}
+                                        }
+                                    }}
+                                }
+                            }}
+                            </td>
+                        }
+                    })}
+                    </tr>
+                }) }
+
+                </table>
+                <div class="button">
+                    <button onclick={ctx.link().callback(move |_| AppMsg::GameNew)}>{"NEW GAME"}</button>
+                </div>
+                </div>
+
+
+                    }
+
+                } else {
+                    html! {
+                        <div>
+
+                            <ul>
+                                { for self.state.players.iter().enumerate().map(|(idx, player)| self.view_player(idx, player, ctx.link()))}
+                            </ul>
+                            {self.view_input(ctx.link())}
+
+
+                <div class="button">
+                    <button onclick={ctx.link().callback(move |_| AppMsg::GameStart)}>{"START GAME"}</button>
+                </div>
+                        </div>
+                    }
+
+                }}
+            </div>
         }
     }
 }
